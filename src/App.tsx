3a66d533afb15622cab80fbe84e752425af29a8c
Abdur Rehman <abdurrehman@codedevza.com>
2025-07-02 14:08:04 +0500

    Update : Default Theme

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <ThemeProvider
       attribute="class"
-      defaultTheme="system"
+      defaultTheme="light"
       enableSystem
       disableTransitionOnChange
     >
